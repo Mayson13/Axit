fdd269faa25ce2f9a8cd71a3a7229fda509d32d9
DrMayson <motrik__xxl@mail.ru>
2022-09-13 00:25:39 +0300

    newProject

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+  ❏ ❐ ❏ ❐ ❏ ❐ ❏ ❐ ❑ ❒ ❏ ❐ ❏ ❐ ❑ ❒ ❏ ❐ ❑ ❒
+  ❐ ********************************************** ❑
+  ❑ *  AUTHOR:                 Dr. Mayson        * ❒
+  ❐ *  GENERATOR:         Visual Studio Code     * ❑
+  ❑ *  WEBSITE:           http://www.***.com/    * ❒
+  ❐ *  E-MAIL:            motrik_xxl@mail.ru     * ❑
+  ❑ *  TEMPLATE NAME:             AXIT           * ❒
+  ❐ *  VERSION:                    1.0           * ❑
+  ❑ *  LAST MODIFIED:      September-12-2022     * ❒
+  ❐ ********************************************** ❑
+  ❑ ❐ ❑ ❒ ❏ ❐ ❏ ❐ ❑ ❒ ❏ ❐ ❑ ❒ ❑ ❒ ❑ ❐ ❑ ❒ 
+*/
+
+
+"use strict";
